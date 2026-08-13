83f8786227909cc8f66485952f1230b8ade66edf
mabreu22 <mauricio.magosoft@gmail.com>
2026-08-13 09:17:48 -0300

    fix: forca visibilidade de display flex em abrirModalCanal e adiciona headers no-cache no servidor

--- Server.js
+++ Server.js
@@ -11,9 +11,13 @@ const PORT = 3000;
 
 // Middlewares
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    next();
+});
 app.use(express.json({ limit: '15mb' }));
 app.use(express.urlencoded({ limit: '15mb', extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { etag: false, lastModified: false }));
 
 // Configuração de Conexão com o Firebird 5.0
 const dbOptions = require('./dbConfig');
